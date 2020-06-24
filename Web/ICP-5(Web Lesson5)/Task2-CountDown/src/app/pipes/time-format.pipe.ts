91c42538a02c0bf77709697e4c7aa1ed5352893c
Yamini <yaminreddy@gmail.com>
2020-06-23 20:58:49 -0500

    updates Done!
    updates Done!

---
+++ Web/ICP-5(Web Lesson5)/Task2-CountDown/src/app/pipes/time-format.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeFormat',
+  pure: true
+})
+export class TimeFormatPipe implements PipeTransform {
+
+  /**
+   * Value is in seconds.
+   */
+  transform(value: any, args?: any): any {
+    // 4.1
+    const hours = Math.floor((value / 60) / 60);
+    const minutes = Math.floor(value / 60) % 60;
+    const seconds = value % 60;
+    const days = Math.floor(value / 3600)
+    return `${this.padding(days)}${days}:${this.padding(hours)}${hours}:${this.padding(minutes)}${minutes}:${this.padding(seconds)}${seconds}`;
+    // END 4.1
+  }
+
+  private padding(time) {
+    return time < 10 ? '0' : '';
+  }
+}
